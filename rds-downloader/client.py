5abb17d61978f430eec05636dc8cf2b7d516f902
nshefeek <connectshefeek@gmail.com>
2020-02-17 10:35:53 +0400

    Added client code

---
+++ rds-downloader/client.py
@@ -0,0 +1,44 @@
+import requests
+import logging
+import boto3
+from botocore.exceptions import ClientError
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--bucket', '--b', help='Bucket Name')
+parser.add_argument('--object', '--o', help='Object Name')
+parser.add_argument('--time', '--t', help='Expiration time',type=int, default=3600)
+args = parser.parse_args()
+print(args)
+
+bucket_name = args.bucket
+object_name = args.object
+expiration = args.time
+
+def create_presigned_url(bucket_name, object_name, expiration=3600):
+    """Generate a presigned URL to share an S3 object
+
+    :param bucket_name: string
+    :param object_name: string
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Presigned URL as string. If error, returns None.
+    """
+
+    # Generate a presigned URL for the S3 object
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.generate_presigned_url('get_object',
+                                                    Params={'Bucket': bucket_name,
+                                                            'Key': object_name},
+                                                    ExpiresIn=expiration)
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+    # The response contains the presigned URL
+    return response
+
+url = create_presigned_url('BUCKET_NAME', 'OBJECT_NAME')
+if url is not None:
+    response = requests.get(url)
+
